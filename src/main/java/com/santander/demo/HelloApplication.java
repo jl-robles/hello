02f6425ba9cd8761aa2b24c29550022ce40ca984
jl-robles <luis.robles731@gmail.com>
2018-03-27 08:58:05 -0600

    updated java

--- src/main/java/com/santander/demo/HelloApplication.java
+++ src/main/java/com/santander/demo/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		System.out.println("***  Hello World App ***");
+		System.out.println("***  Hello World App 001***");
 		
 		boolean var = false;
 		System.out.println("Var value: "+var);		
