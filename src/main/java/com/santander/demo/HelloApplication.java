030ba42063978442fd07e36e178a8e15fefe69c9
jl-robles <luis.robles731@gmail.com>
2018-03-27 10:37:00 -0600

    Updated from testing

--- src/main/java/com/santander/demo/HelloApplication.java
+++ src/main/java/com/santander/demo/HelloApplication.java
@@ -1,16 +1,21 @@
 package com.santander.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(HelloApplication.class);
+
 
 	public static void main(String[] args) {
-		System.out.println("***  Hello World App 002***");
+		log.info("***  Hello World App 003***");
 		
 		boolean var = true;
-		System.out.println("Var value: "+var);		
+		log.info("Var value: "+var);		
 		
 		SpringApplication.run(HelloApplication.class, args);
 	}
